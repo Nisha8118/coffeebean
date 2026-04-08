fed00957aca5c1b0d4fdd8172024a2bc4bd8854a
Nisha8118 <nishakn8118@gmail.com>
2026-04-08 16:03:03 +0530

    Change rendered template from index.html to body.html

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html', title="Home Page")
+    return render_template('body.html', title="Home Page")
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
